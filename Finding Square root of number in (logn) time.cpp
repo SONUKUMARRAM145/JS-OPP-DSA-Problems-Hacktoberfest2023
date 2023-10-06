640f19760e75cf55268bbf44e5a40b4c3b896912
SONU KUMAR RAM <92182339+SONUKUMARRAM145@users.noreply.github.com>
2023-10-06 11:53:44 +0530

    Create Finding Square root of number in (logn) time.cpp
    Using This approach one can find sqrt of number in  O(logn)

---
+++ Finding Square root of number in (logn) time.cpp
@@ -0,0 +1,22 @@
+// Using Binary Search Finding Square root of number
+int floorsqrt(int n)
+{
+//   if (n == 0 || n == 1) {
+//     return n;
+//   }
+    int low=1;
+    int high=n;
+    int ans=1;
+    while(low<=high){
+        long long mid=(low+high)/2;
+        long long val=(mid*mid);
+        if(val<=n){
+            ans=mid;
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return high;
+}
